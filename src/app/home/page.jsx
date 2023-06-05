604bc041164fe19694283bfbf074d5567709ff3c
Md Yamin <mdyamin007@yahoo.com>
2023-06-05 23:58:46 +0600

    elapsed time added

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -3,47 +3,73 @@
 import { redirect } from "next/navigation";
 import { useAuth } from "../context/authContext";
 import { database } from "@/firebase/config";
-import { equalTo, get, onValue, orderByChild, push, query, ref, remove, set } from "firebase/database";
+import {
+  equalTo,
+  get,
+  onValue,
+  orderByChild,
+  push,
+  query,
+  ref,
+  remove,
+  set,
+} from "firebase/database";
 import { useEffect, useState } from "react";
+import moment from "moment";
 
 const Home = () => {
-  const [totalOccupations, setTotalOccupations] = useState(0)
-  const [isCheckedIn, setIsCheckedin] = useState(false)
-  const [checking, setChecking] = useState(false)
-  const [checkedInTime, setCheckedInTime] = useState(0)
+  const [totalOccupations, setTotalOccupations] = useState(0);
+  const [isCheckedIn, setIsCheckedin] = useState(false);
+  const [checking, setChecking] = useState(false);
+  const [checkedInTime, setCheckedInTime] = useState("");
+  const [elapsedTime, setElapsedTime] = useState("");
   const { logout, currentUser, loading } = useAuth();
-  const [elapsedTime, setElapsedTime] = useState("")
+
+  // console.log(moment().format());
 
   useEffect(() => {
     const getNumberOfOccupants = () => {
-      const occupantsRef = ref(database, "occupants")
+      const occupantsRef = ref(database, "occupants");
       onValue(occupantsRef, (snapshot) => {
-        const data = snapshot.val()
+        const data = snapshot.val();
         // console.log(Object.keys(snapshot.val()).length);
-        if (data)
-          setTotalOccupations(Object.keys(data).length)
-        else setTotalOccupations(0)
-      })
-    }
+        if (data) setTotalOccupations(Object.keys(data).length);
+        else setTotalOccupations(0);
+      });
+    };
     const checkCheckedInOrNot = async () => {
-      setChecking(true)
+      setChecking(true);
       if (currentUser) {
-        const userRef = query(ref(database, "occupants"), orderByChild("email"), equalTo(currentUser.email))
+        const userRef = query(
+          ref(database, "occupants"),
+          orderByChild("email"),
+          equalTo(currentUser.email)
+        );
         onValue(userRef, (snapshot) => {
-          const data = snapshot.val()
+          const data = snapshot.val();
           if (data) {
-            setIsCheckedin(true)
-
-          }
-          else setIsCheckedin(false)
+            setIsCheckedin(true);
+            // console.log(Object.values(data)[0]);
+            setCheckedInTime(Object.values(data)[0].timestamp);
+          } else setIsCheckedin(false);
           // console.log(data);
-        })
-        setChecking(false)
+        });
+        setChecking(false);
       }
-    }
-    checkCheckedInOrNot()
+    };
+    checkCheckedInOrNot();
     getNumberOfOccupants();
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const timeDiff = moment(checkedInTime).fromNow(true);
+
+      setElapsedTime(timeDiff);
+    }, 1000); // Update every second
+
+    return () => clearInterval(interval); // Clean up the interval on unmount
+  }, [checkedInTime]);
 
   if (!currentUser) {
     redirect("/");
@@ -51,42 +77,69 @@ const Home = () => {
 
   const handleCheckIn = () => {
     if (currentUser) {
-      const timestamp = Date.now();
+      const timestamp = moment().format();
       // console.log(timestamp);
-      const occupantsRef = ref(database, "occupants")
-      const newOccupantRef = push(occupantsRef)
+      const occupantsRef = ref(database, "occupants");
+      const newOccupantRef = push(occupantsRef);
       let data = {
         name: currentUser.displayName,
         email: currentUser.email,
         photo: currentUser.photoURL,
         timestamp: timestamp,
-      }
+      };
       set(newOccupantRef, data);
     }
-  }
+  };
 
   const handleCheckOut = () => {
     if (currentUser) {
       console.log("Checking out");
-      const userRef = query(ref(database, "occupants"), orderByChild("email"), equalTo(currentUser.email))
-      remove(userRef)
+      const userRef = query(
+        ref(database, "occupants"),
+        orderByChild("email"),
+        equalTo(currentUser.email)
+      );
+      remove(userRef);
+      setCheckedInTime("");
+      setIsCheckedin(false);
     }
-  }
+  };
 
   return (
     <>
-      {checking ? (<p>Loading....</p>) : (<div className="min-h-screen flex flex-col items-center justify-center">
-        {
-          loading ? (
-            <p> Loading...</p >
+      {checking ? (
+        <p>Loading....</p>
+      ) : (
+        <div className="min-h-screen flex flex-col items-center justify-center">
+          {loading ? (
+            <p> Loading...</p>
           ) : (
             <div>
               <p className="my-2">Total occupancy: {totalOccupations}</p>
-              <p className="my-2">{elapsedTime} in Cafe!</p>
-              {!isCheckedIn ? (<button className="bg-green-400 px-2 py-2 text-white" onClick={handleCheckIn}>Check in</button>) : (<button className="bg-red-400 px-2 py-2 text-white" onClick={handleCheckOut}> Check out?</button>)}
+              {checkedInTime !== "" && (
+                <p className="my-2">{elapsedTime} in Cafe!</p>
+              )}
+              {!isCheckedIn ? (
+                <button
+                  className="bg-green-400 px-2 py-2 text-white"
+                  onClick={handleCheckIn}
+                >
+                  Check in
+                </button>
+              ) : (
+                <button
+                  className="bg-red-400 px-2 py-2 text-white"
+                  onClick={handleCheckOut}
+                >
+                  {" "}
+                  Check out?
+                </button>
+              )}
               <button onClick={logout}>Log out</button>
-            </div>)}
-      </div >)}
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 };
