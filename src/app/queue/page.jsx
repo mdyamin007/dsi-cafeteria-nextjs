648d4edc37c715cb2b81cb68fa6da2aacddabd78
mdyamin007 <mdyamin007@yahoo.com>
2023-06-12 15:30:22 +0600

    cards added in user queue page

--- src/app/queue/page.jsx
+++ src/app/queue/page.jsx
@@ -30,18 +30,19 @@ const Queue = () => {
     }
 
     return (
-        <div className="my-2">
-            <div className="flex flex-col items-center justify-center">
+        <div className="w-full my-2 text-center">
+            <div className="w-full flex flex-wrap gap-4 my-10 md:flex-row items-center justify-center">
                 {occupantList && occupantList.map(occupant => (
-                    <div className="mx-auto md:mx-auto w-full md:w-1/2 my-2 border shadow-md px-3 py-4 flex flex-col gap-2 md:gap-0 md:flex-row items-center justify-around" key={occupant.uid}>
+                    <div className="w-full md:w-1/6 my-2 border shadow-md px-3 py-8 flex flex-col space-y-2 items-center justify-around" key={occupant.uid}>
                         <Image src={occupant.photoURL} alt="User picture" width={50} height={50} className="rounded-full" />
                         <p>{occupant.name}</p>
                         <p>{moment(occupant.timestamp).fromNow(true)}</p>
                     </div>
                 ))}
-                <Link href="/home" className="my-10 md:my-24 text-blue-600">Back to check in page</Link>
             </div>
+            <Link href="/home" className="my-14 md:my-24 text-blue-600">Back to check-in page</Link>
         </div>
+
     )
 }
 
